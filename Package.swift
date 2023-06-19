e99d1e04d44b921f54dc52cdeaa5f4e23c154d9c
Théodore Roos <25298135+TheodoreRoos@users.noreply.github.com>
2023-06-19 13:35:29 +0200

    Changed version for foundation package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["VSWiFi"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/virtualstores/ios-foundation.git", .exact("0.0.13")),
+        .package(url: "https://github.com/virtualstores/ios-foundation.git", .upToNextMajor(from: "0.0.16")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
